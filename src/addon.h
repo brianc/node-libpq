f674e0df0ae83db42bdb0b7f88db1fd711dca020
Brian M. Carlson <brian.m.carlson@gmail.com>
2014-10-15 23:16:51 -0400

    Change order of definition

--- src/addon.h
+++ src/addon.h
@@ -5,12 +5,13 @@
 #include <nan.h>
 #include <libpq-fe.h>
 #include <pg_config.h>
-#include "connection.h"
-#include "connect-async-worker.h"
 
 #if PG_VERSION_NUM > 90000
 #define ESCAPE_SUPPORTED
 #endif
+
+#include "connection.h"
+#include "connect-async-worker.h"
 
 //#define LOG(msg) printf("%s\n", msg);
 //#define TRACEF(format, arg) printf(format, arg);
