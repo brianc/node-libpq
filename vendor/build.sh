abc732173db908495c80bfe271b8e95a2c6f5431
Gary Mardell <info@garymardell.co.uk>
2024-05-24 16:50:34 -0700

    Update node versions and openssl

--- vendor/build.sh
+++ vendor/build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 BUILD_DIR="$(pwd)"
-source ./vendor/install_openssl.sh 1.1.1b
+source ./vendor/install_openssl.sh 3.0.13+quic
 sudo updatedb
 source ./vendor/install_libpq.sh
 sudo updatedb
